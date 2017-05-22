099fb498b4cd8aca106e827ebe953a4117aa367b
octaharon <neolord.tmb@gmail.com>
2017-05-22 14:28:47 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Хост: 127.0.0.1
--- Время создания: Апр 18 2017 г., 06:35
--- Версия сервера: 5.6.17
--- Версия PHP: 5.6.15
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- База данных: `toptal`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `auth_token`
---
 
 CREATE TABLE IF NOT EXISTS `auth_token` (
   `token` varchar(32) NOT NULL,
@@ -33,12 +8,6 @@ CREATE TABLE IF NOT EXISTS `auth_token` (
   PRIMARY KEY (`token`),
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `runs`
---
 
 CREATE TABLE IF NOT EXISTS `runs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -50,12 +19,6 @@ CREATE TABLE IF NOT EXISTS `runs` (
   KEY `user_id` (`user_id`),
   KEY `date` (`date`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `users`
---
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
